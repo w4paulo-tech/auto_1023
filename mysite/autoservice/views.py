f4801bec6d3fd7051074030e27d2963b6fb3c47e
Paulius Kavaliauskas <w4paulo@gmail.com>
2025-11-03 16:33:54 +0200

    pridėta vartotojo prisijungimas

--- mysite/autoservice/views.py
+++ mysite/autoservice/views.py
@@ -5,10 +5,13 @@ from django.core.paginator import Paginator
 from django.db.models import Q
 
 def index(request):
+    num_visits = request.session.get('num_visits', 1)
+    request.session['num_visits'] = num_visits + 1
     my_context = {
         'num_paslaugos': Paslauga.objects.count(),
         'num_atlikti_uzsakymai': Uzsakymas.objects.filter(statusas='a').count(),
-        'num_automobiliai': Automobilis.objects.count()
+        'num_automobiliai': Automobilis.objects.count(),
+        'num_visits': num_visits,
 
     }
     return render(request, template_name="index.html", context=my_context)
@@ -53,8 +56,12 @@ def paieska(request):
         Q(license_plate__icontains=query) |
         Q(vin_code__icontains=query)
     )
+    paginator = Paginator(auto_paieskos_rezultatai, per_page=1)
+    page_number = request.GET.get('page')
+    paged_search = paginator.get_page(page_number)
     context = {
         "query": query,
         "autos": auto_paieskos_rezultatai,
+        "search": paged_search,
     }
     return render(request, template_name="paieska.html", context=context)
--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -12,7 +12,7 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 
 from pathlib import Path
 import os
-from .my_settings import SECRET_KEY, DEBUG, ALLOWED_HOSTS
+from .my_settings import SECRET_KEY, DEBUG, ALLOWED_HOSTS, EMAIL_HOST_PASSWORD
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -130,3 +130,16 @@ MEDIA_URL = '/media/'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+LOGIN_REDIRECT_URL = '/'
+LOGOUT_REDIRECT_URL = '/'
+
+EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_POST = 587
+EMAIL_USE_TLS = True
+EMAIL_HOST_USER = 'mano_pastas@gmail.com'
+# el. pašto adresas iš kurio siųsite
+EMAIL_HOST_PASSWORD = EMAIL_HOST_PASSWORD
+
+# slaptažodis
